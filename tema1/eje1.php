fb1e6270365266cb604be22ea56252886cbaac24
Luisj122 <luisjmnz122@gmail.com>
2022-09-27 11:53:23 +0200

    ac

--- tema1/eje1.php
+++ tema1/eje1.php
@@ -1,16 +1,14 @@
 <?php
-    include_once($_SERVER["DOCUMENT_ROOT"] . "/cabecera.php");
-?>
+    
+	//Inicializamos las variable y usamos rand para poder generar numeros aleatorios
+	$pimera = rand();
+	$segunda = rand();
 
-      <div class="col-md-8 themed-grid-col">
-		<div class="flex-shrink-0 p-3 bg-white">
-			<?php
-				echo "Aquí vendría el código del ejercicio1 que estoy haciendo";
-			?>
-		</div>
-	  </div>
-    </div>
+	$resta = $pimera - $segunda;
+	$division = $pimera / $segunda;
 
-<?php
-    include_once("../pie.php");
+	echo "La resta = $resta <br>";
+	echo "La division = $division ";
+
+	
 ?>

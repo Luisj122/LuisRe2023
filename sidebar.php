fb33e3193eb2a436859bdf8274c97172a3c649e0
Luisj122 <luisjmnz122@gmail.com>
2022-10-30 11:48:05 +0100

    ejer

--- sidebar.php
+++ sidebar.php
@@ -62,7 +62,7 @@
 					</button>
 					<div class="collapse" id="tema3">
 					<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-					
+					<li><a href="/tema3/practicaLogin/login.php" class="link-dark rounded">Practica Login</a></li>
 					
 					</ul>
 					</div>
